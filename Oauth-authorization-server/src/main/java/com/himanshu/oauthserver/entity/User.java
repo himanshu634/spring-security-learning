c496f9e95030fd82eeff33534b58fb1ce5df0d59
Himashu Mendapara <himanshumendapra@gmail.com>
2022-07-25 22:26:02 +0530

    Added OAuth2.0

---
+++ Oauth-authorization-server/src/main/java/com/himanshu/oauthserver/entity/User.java
@@ -0,0 +1,144 @@
+package com.himanshu.oauthserver.entity;
+
+import javax.persistence.*;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(
+            strategy = GenerationType.IDENTITY
+    )
+    private Long userId;
+    private String firstName;
+    private String lastName;
+    private String email;
+    @Column(
+            length = 60
+    )
+    private String password;
+    private String role;
+    private boolean enabled = false;
+
+    public User(Long userId, String firstName, String lastName, String email, String password, String role, boolean enabled) {
+        this.userId = userId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+        this.enabled = enabled;
+    }
+
+    public User() {
+    }
+
+    public Long getUserId() {
+        return this.userId;
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public String getRole() {
+        return this.role;
+    }
+
+    public boolean isEnabled() {
+        return this.enabled;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public boolean equals(final Object o) {
+        if (o == this) return true;
+        if (!(o instanceof User)) return false;
+        final User other = (User) o;
+        if (!other.canEqual((Object) this)) return false;
+        final Object this$userId = this.getUserId();
+        final Object other$userId = other.getUserId();
+        if (this$userId == null ? other$userId != null : !this$userId.equals(other$userId)) return false;
+        final Object this$firstName = this.getFirstName();
+        final Object other$firstName = other.getFirstName();
+        if (this$firstName == null ? other$firstName != null : !this$firstName.equals(other$firstName)) return false;
+        final Object this$lastName = this.getLastName();
+        final Object other$lastName = other.getLastName();
+        if (this$lastName == null ? other$lastName != null : !this$lastName.equals(other$lastName)) return false;
+        final Object this$email = this.getEmail();
+        final Object other$email = other.getEmail();
+        if (this$email == null ? other$email != null : !this$email.equals(other$email)) return false;
+        final Object this$password = this.getPassword();
+        final Object other$password = other.getPassword();
+        if (this$password == null ? other$password != null : !this$password.equals(other$password)) return false;
+        final Object this$role = this.getRole();
+        final Object other$role = other.getRole();
+        if (this$role == null ? other$role != null : !this$role.equals(other$role)) return false;
+        if (this.isEnabled() != other.isEnabled()) return false;
+        return true;
+    }
+
+    protected boolean canEqual(final Object other) {
+        return other instanceof User;
+    }
+
+    public int hashCode() {
+        final int PRIME = 59;
+        int result = 1;
+        final Object $userId = this.getUserId();
+        result = result * PRIME + ($userId == null ? 43 : $userId.hashCode());
+        final Object $firstName = this.getFirstName();
+        result = result * PRIME + ($firstName == null ? 43 : $firstName.hashCode());
+        final Object $lastName = this.getLastName();
+        result = result * PRIME + ($lastName == null ? 43 : $lastName.hashCode());
+        final Object $email = this.getEmail();
+        result = result * PRIME + ($email == null ? 43 : $email.hashCode());
+        final Object $password = this.getPassword();
+        result = result * PRIME + ($password == null ? 43 : $password.hashCode());
+        final Object $role = this.getRole();
+        result = result * PRIME + ($role == null ? 43 : $role.hashCode());
+        result = result * PRIME + (this.isEnabled() ? 79 : 97);
+        return result;
+    }
+
+    public String toString() {
+        return "User(userId=" + this.getUserId() + ", firstName=" + this.getFirstName() + ", lastName=" + this.getLastName() + ", email=" + this.getEmail() + ", password=" + this.getPassword() + ", role=" + this.getRole() + ", enabled=" + this.isEnabled() + ")";
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Spring security Learning
 Learning spring-security by implementing some basic user login and register features.
+<h3>Learnings :</h3>
+***
+<ul>
+    <li>How to use java spring data JPA</li>
+    <li>How to implement OAuth2.0</li>
+    <li>How to build login/register API</li>
+</ul>
